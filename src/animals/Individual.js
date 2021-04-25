6d4c789caf34496d5a2648c0c72b240d9b0972dd
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-04-25 15:42:08 -0400

    styled feed meter

--- src/animals/Individual.js
+++ src/animals/Individual.js
@@ -8,23 +8,36 @@ class Individual extends Component{
     this.state = {
       rename: false
     }
+    this.handleRename = this.handleRename.bind(this);
     this.handleFeed = this.handleFeed.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
-  componentDidUpdate() {
-    console.log(!this.state.rename);
+  handleRename() {
+    this.setState({
+      rename: true
+    });
+  }
+  handleCancel() {
+    this.setState({
+      rename: false
+    })
   }
   handleFeed() {
     this.props.feedAnimal(this.props.info, this.props.species);
   }
+
   render() {
-    const living = (alive) => {
-      if (alive) {
+    const living = () => {
+      if (this.props.info.alive) {
         return(
           <div className='alive'>
-            {renameDisplay(this.props.rename)}
+            {renameDisplay()}
             <div className='individual-food-meter'>
-              <div>{this.props.info.hungerMeter}</div>
-              <button className='btn btn-info' onClick={this.handleFeed}>Feed ({this.props.foodConsumption})</button>
+              <div className='meter-container'>
+                <div className='meter' style={{width: `${this.props.info.hungerMeter}%`}}></div>
+                <div className='meter-text'>{this.props.info.hungerMeter}/100</div>
+              </div>
+              <button className='btn btn-info feed-btn' onClick={this.handleFeed}>Feed ({this.props.foodConsumption})</button>
             </div>
           </div>
         )
@@ -39,17 +52,25 @@ class Individual extends Component{
         )
       }
     }
-    const renameDisplay = (rename) => {
-      if (!rename) {
+    const renameDisplay = () => {
+      if (this.state.rename) {
         return (
-          <div className='renameDisplay'>
+          <div className='renaming'>
+            <input></input>
+            <button>Confirm</button>
+            <button className='btn btn-warning' onClick={this.handleCancel}>Cancel</button>
+          </div>  
+        )
+      } else {
+        return(
+          <div className='name-and-image'>
             <div className='changeIndividualName'>
-              <button className='btn btn-sm btn-secondary'>Rename</button>
+              <button className='btn btn-sm btn-secondary' onClick={this.handleRename}>Rename</button>
             </div>
             <div className='imgDisplay'>
-              <img src={this.props.imgUrl} className='imgDisplay' alt={`${this.props.species} picture`}/>
+              <img src={this.props.imgUrl} className='imgDisplay' alt={`${this.props.species}`}/>
             </div>
-         </div> 
+          </div> 
         )
       }
     }
@@ -58,7 +79,7 @@ class Individual extends Component{
         <div className='individual-name'>
           <h6>{this.props.info.name}</h6>
         </div>
-        {living(this.props.info.alive)}
+        {living()}
       </div>
     )
   }
